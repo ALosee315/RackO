469ec4dce569fa35c9e458d5ec5956756bbac315
ALosee315 <77549989+ALosee315@users.noreply.github.com>
2022-04-01 22:47:21 -0400

    Add files via upload
    Added comments

--- RackO.java
+++ RackO.java
@@ -14,50 +14,50 @@ public class RackO {
 
     static int[] plr1Rack = new int[10];
     static int[] plr2Rack = new int[10];
-    static int[] deckOCards = new int[61];
-    static int[] discardPile = new int[40];
+    static int[] deckOCards = new int[61]; //Deck of cards
+    static int[] discardPile = new int[40]; //Creates empty discard pile
     static String plr1Name;
     static String plr2Name;
     static int discard;
     static int draw;
     static Random rand = new Random();
     static Scanner input = new Scanner(System.in);
-    static int turn = 1;
+    static int turn = 1; //Player 1 is up first by default
 
     public static void removeCard(int selectedCard) {
         for (int i = selectedCard; i < deckOCards.length - 1; i++) { //Remove card from array
-            deckOCards[i] = deckOCards[i + 1];
+            deckOCards[i] = deckOCards[i + 1]; //Shift cards after the card removed in the array
             //System.out.println(deckOCards[i]);
         }
 
         System.out.println("");
     }
 
-    public static void checkArray() {
+    public static void checkArray() { //Method for testing purposes, shows what cards are left in the deck
         for (int i = 0; i < deckOCards.length; i++) { 
             System.out.println(deckOCards[i]);
         }
 
     }
 
-    public static int selectCard() {
+    public static int selectCard() {    //Draw a random card from the deck
         int selectedCard = rand.nextInt(deckOCards.length);
         if (deckOCards[selectedCard] == 0) {
-            while (deckOCards[selectedCard] == 61 || deckOCards[selectedCard] == 0) {
+            while (deckOCards[selectedCard] == 61 || deckOCards[selectedCard] == 0) { //Make sure card drawn isn't out of limit
                 selectedCard = rand.nextInt(deckOCards.length);
             }
         }
         if (deckOCards[selectedCard] != 0) {
             return selectedCard;
         } else {
-            System.out.println("Error!!!");
+            System.out.println("Error!!!"); //If for some reason the code starts drawing 0s
             return -1;
         }
 
     }
 
     public static void checkForShuffle() {
-        if (deckOCards[0]==0) {
+        if (deckOCards[0]==0) { //If there are no cards left in the deck
             deckOCards = new int[40];
             for (int i = 0; i < discardPile.length; i++) { //Generate deck, 1-40
                 deckOCards[i] = discardPile[i]; //Transfer Discard Pile into reshuffled draw deck
@@ -79,21 +79,21 @@ public class RackO {
             deckOCards[i] = i + 1;
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {  //Generate both players' racks
             int selectedCard1 = selectCard();
             plr1Rack[i] = deckOCards[selectedCard1];
             removeCard(selectedCard1);
-            System.out.println(selectedCard1);
+            //System.out.println(selectedCard1);
             int selectedCard2 = selectCard();
             plr2Rack[i] = deckOCards[selectedCard2];
             removeCard(selectedCard2);
             //System.out.println(plr1Rack[i]+"\t"+plr2Rack[i]);
         }
         int selectedCard3 = selectCard();
-        discard = deckOCards[selectedCard3];
+        discard = deckOCards[selectedCard3]; //Set first discard
         removeCard(selectedCard3);
         int selectedCard4 = selectCard();
-        draw = deckOCards[selectedCard4];
+        draw = deckOCards[selectedCard4];   //Set first card to be drawn
         removeCard(selectedCard4);
     }
 
@@ -126,32 +126,32 @@ public class RackO {
             for (int i = 9; i >= 0; i--) { //Runs backwards through the table making sure that the rack is increasing upwards
                 if (i > 0) {
                     if (plr1Rack[i] > plr1Rack[i - 1]) {
-                        return (winner);
+                        return (winner);    //No one wins yet
                     }
 
                 }
             }
             winner = 1;
-            return (winner);
+            return (winner);    //Plr 2 wins
         } else {
             for (int i = 9; i >= 0; i--) {
                 if (i > 0) {
                     if (plr2Rack[i] < plr2Rack[i - 1]) {
-                        return (winner);
+                        return (winner); //No one wins yet
                     }
 
                 }
             }
             winner = 2;
-            return (winner);
+            return (winner);    //Plr two wins
         }
 
     }
 
     public static void main(String[] args) {
 
-        int cardHolding;
-        int userChoice;
+        int cardHolding;    //The card that the player picks up
+        int userChoice;     //The user's input value
 
         dealCards();
         //System.out.println("Discard: "+discard);
@@ -174,7 +174,7 @@ public class RackO {
                 if (userChoice == 1) {
                     cardHolding = discard;
                 } else if (userChoice == 3) {
-                    checkArray(); //TEST
+                    checkArray(); //################TEST################//
                     cardHolding = draw;
                 } else {
                     checkForShuffle();
@@ -192,7 +192,7 @@ public class RackO {
                     discard = cardHolding;
                 
                 } else {
-                    insertDiscard(discard);
+                    insertDiscard(discard); //Replace the card in the rack
                     discard = plr1Rack[userChoice];
                     plr1Rack[userChoice] = cardHolding;
                 }
@@ -223,7 +223,7 @@ public class RackO {
                 if (userChoice == 1) {
                     cardHolding = discard;
                 } else if (userChoice == 3) {
-                    checkArray(); //TEST
+                    checkArray(); //################TEST################//
                     cardHolding = draw;
                 } else {
                     checkForShuffle();
@@ -240,7 +240,7 @@ public class RackO {
                     insertDiscard(discard);
                     discard = cardHolding;
                 } else {
-                    insertDiscard(discard);
+                    insertDiscard(discard);  //Replace the card in the rack
                     discard = plr2Rack[userChoice];
                     plr2Rack[userChoice] = cardHolding;
                 }
